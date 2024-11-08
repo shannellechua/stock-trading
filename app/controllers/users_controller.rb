a87daa334b92a5040ba5a718c1e77d2bd5f93352
Shannelle <shannellechua@gmail.com>
2024-11-08 20:43:54 +0800

    Added editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def user_params
-      params.expect(user: [:first_name, :last_name, :email, :password, :balance])
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :balance)
     end
 end
