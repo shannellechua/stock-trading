6f87d420af897718ef66f89c1a5f68097c610b6b
Shannelle <shannellechua@gmail.com>
2024-11-09 09:46:07 +0800

    Added admin namespace

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user
+      redirect_to admin_users_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to @user
+      redirect_to admin_user_path
     else
       render :edit, status: :unprocessable_entity
     end
