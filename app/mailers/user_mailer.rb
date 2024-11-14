e0e18e184a7941e2d0acec6be812ad34b2c10591
Evandel Maylas <evandel@avionschool.com>
2024-11-14 19:38:21 +0800

    changes to schema

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
     default from: "shannellechua@gmail.com"
     layout 'mailer'
     
-    def approve _email(user)
+    def approve_email(user)
       @user = user
       mail(to: @user.email, subject: "You're Ready for Trading!")
     end
